47e5bb7fd227f8afe0970c7c970f15d61cf086f9
MeFerdi <ferdinandodhis254@gmail.com>
2025-12-01 21:07:18 +0300

    fix: revoke secret keys

--- A2SVTracker/settings.py
+++ A2SVTracker/settings.py
@@ -12,16 +12,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-
+import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%6ody+57o!itt-pchntus@=6i*ur=6b#l6+ad*5!slaoj*93j5'
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY") or "django-insecure-dev-key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
