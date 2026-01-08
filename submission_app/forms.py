ea085ccf56d98cc7e37de0781b61daa55109c21f
MeFerdi <ferdinandodhis254@gmail.com>
2026-01-09 02:57:46 +0300

    feat: update SubmissionForm widget styling for improved user experience

--- submission_app/forms.py
+++ submission_app/forms.py
@@ -66,7 +66,7 @@ class SubmissionForm(forms.ModelForm):
         fields = ['submission_link']
         widgets = {
             'submission_link': forms.URLInput(attrs={
-                'class': 'form-input',
+                'class': 'w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500',
                 'placeholder': 'https://leetcode.com/submissions/...'
             })
         }
